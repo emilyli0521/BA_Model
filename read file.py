189bba7919d2c178f8fbcedd5017d7e19c9cc272
emilyli0521 <a0930591669@gmail.com>
2024-07-03 17:59:43 +0800

    update

--- read file.py
+++
@@ -1,6 +0,0 @@
-with open("./corpus.txt", mode= "w", encoding = "utf-8") as i:
-    i.write("Hello")
-
-with open("./corpus.txt", mode= "r", encoding ="utf-8") as f:
-   contents = f.read()
-   print(contents)
